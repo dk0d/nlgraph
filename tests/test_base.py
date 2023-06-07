f1921fe2fa2c623ddaf12c1821ae37a0fc666517
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2023-06-07 11:01:36 -0400

    fix: remove pdm.lock move to hatch build system, add hatch run scripts for pyg installation

--- tests/test_base.py
+++ tests/test_base.py
@@ -2,14 +2,12 @@ import nlgraph as ng
 from pathlib import Path
 from nlgraph.graph import Graph
 
-RESOURCES_DIR = Path('./tests/resources').resolve()
-NETLIST_DIR = RESOURCES_DIR / 'netlists'
+RESOURCES_DIR = Path("./tests/resources").resolve()
+NETLIST_DIR = RESOURCES_DIR / "netlists"
+
 
 def test_parse():
-    vPath = NETLIST_DIR / 'netlist_b01.v'
-    graph = Graph.fromVerilog(
-        filePath=vPath
-    )
-    print('testing parsing')
-
-
+    vPath = NETLIST_DIR / "netlist_b01.v"
+    graph = Graph.fromVerilog(filePath=vPath)
+    assert graph is not None
+    # print("testing parsing")
